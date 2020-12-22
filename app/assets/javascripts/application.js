10a9a6605165800319f7fc4070d3aea620be7653
ryo27-jp <calvee.20@gmail.com>
2020-12-22 22:18:38 +0900

    モーダル表示まで

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require rails-ujs
 //= require jquery3
 //= require popper
-//= require bootstrap
+//= require bootstrap-sprockets
 //= require activestorage
 //= require turbolinks
 //= require_tree .
